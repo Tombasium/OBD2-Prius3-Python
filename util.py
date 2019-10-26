40244b0bfcacb6387576d3bb6aa61c0206b03eae
Tom W <tommwalton@gmail.com>
2019-10-26 21:49:20 +0100

    A few mods

--- util.py
+++ util.py
@@ -5,6 +5,13 @@ Created on Sat Oct 26 18:17:00 2019
 @author: TomLap
 """
 
+def hex_to_dec(value):
+    if ord(value[0]) in range(48, 58):
+        decimal = ord(value[0]) - 48
+    if ord(value[0]) in range(65, 71):
+        decimal = ord(value[0]) - 55
+    return int(decimal)
+
 def hex_byte_to_dec_11bit(value):
     
     byte_1 = hex_to_dec(value[0])
@@ -14,3 +21,12 @@ def hex_byte_to_dec_11bit(value):
     value = (byte_1 * 256) + (multiplier * 16) + addition
     
     return value
+
+def hex_to_dec_variable_length(value):
+    result = 0
+    multiplier = 1
+    for i in range(len(value) - 1, -1, -1):
+        result += hex_to_dec(value[i]) * multiplier
+        multiplier = multiplier * 16
+    return result
+    
